54255526718cb9da18249204c1360f1b2d11e9f8
BigBonny <koukouloukil@gmail.com>
2026-05-26 12:20:21 +0200

    Install GTM-KV6FGZ7L, fix lang to fr, add robots.txt and sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./CartContext";
@@ -28,10 +29,30 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KV6FGZ7L');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KV6FGZ7L"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://diesel-injecteurs.com';
+
+  // Static pages
+  const staticPages = [
+    '',
+    '/produits',
+    '/a-propos',
+    '/retour-consigne',
+    '/cgv',
+    '/mentions-legales',
+    '/livraison',
+    '/garantie',
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily' as const,
+    priority: route === '' ? 1 : 0.8,
+  }));
+
+  return staticPages;
+}
