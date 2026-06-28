d2c5f68ab2cf24e9c7e9ddc0196b17c484821c46
Diesel Injecteurs <diesel.injecteurs@gmail.com>
2026-06-28 04:25:20 +0200

    fix: logo h-24/h-32 w-auto (taller = wider since rectangular)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -141,7 +141,7 @@ function NavigationInner() {
                 <img 
                   src="/assets/logo.png" 
                   alt="Injection Diesel Logo" 
-                  className="h-16 sm:h-20 w-auto"
+                  className="h-24 sm:h-32 w-auto"
                 />
               </Link>
 
